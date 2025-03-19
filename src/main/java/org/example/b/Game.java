24d044faee10d84a98d5b16ed73dbbbc7367507b
OnatMrtl <153172352+OnatMrtl@users.noreply.github.com>
2025-03-19 11:47:09 +0300

    A

---
+++ src/main/java/org/example/b/Game.java
@@ -0,0 +1,45 @@
+package org.example.b;
+
+public class Game {
+    private String gameName;
+    private String gameGenre;
+    private String developerName;
+    private String publisherName;
+    private int releaseYear;
+    private float hoursPlayed;
+    private String gameInfo;
+    private int SteamID;
+    private float publicRating;
+    private String coverPath;
+
+    public Game(String gameName,String gameGenre, String developerName, String publisherName, int releaseYear, float hoursPlayed, String gameInfo, int SteamID, float publicRating, String coverPath) {
+        this.gameInfo = gameInfo;
+        this.gameName = gameName;
+        this.gameGenre = gameGenre;
+        this.hoursPlayed = hoursPlayed;
+        this.publisherName = publisherName;
+        this.releaseYear = releaseYear;
+        this.developerName = developerName;
+        this.SteamID = SteamID;
+        this.publicRating = publicRating;
+        if (coverPath != null && new java.io.File(coverPath.replace("file:", "")).exists()) {
+            this.coverPath = coverPath;
+        } else {
+            this.coverPath = "file:src/main/Cover Arts/NemaFoto.jpg";
+        }
+    }
+
+    public float getHoursPlayed() { return hoursPlayed; }
+    public String getGameInfo() { return gameInfo; }
+    public String getGameName() { return gameName; }
+    public String getPublisherName() { return publisherName; }
+    public int getReleaseYear() { return releaseYear; }
+    public String getDeveloperName() { return developerName; }
+    public int getSteamID() { return SteamID; }
+    public float getPublicRating() { return publicRating; }
+    public String getCoverPath() { return coverPath; }
+    public String getGameGenre() { return gameGenre; }
+    public String getYearString(){
+        return String.valueOf(getReleaseYear());
+    }
+}
