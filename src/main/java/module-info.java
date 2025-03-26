ba8cb0137ba0da8d6249a5eab7b7c53194238bff
OnatMrtl <153172352+OnatMrtl@users.noreply.github.com>
2025-03-26 14:26:26 +0300

    Update module-info.java
    Proje bitti lan

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.b {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires com.google.gson;
 
     opens org.example.b to javafx.fxml;
     exports org.example.b;
