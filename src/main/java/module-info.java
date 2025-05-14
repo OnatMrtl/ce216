54d1172c11f9e06b6fcf5315bdc6c969d9b61a30
OnatMrtl <153172352+OnatMrtl@users.noreply.github.com>
2025-05-14 19:19:54 +0300

    HAHAHAHAHAHAH

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module org.example.b {
+    requires javafx.base;
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.web;
+    requires javafx.swing;
     requires java.desktop;
     requires com.google.gson;
-    opens org.example.b to javafx.fxml,com.google.gson;
+
+    opens org.example.b to javafx.fxml, com.google.gson;
     exports org.example.b;
 }
